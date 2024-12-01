e4b18b099549c1178f56dc58f2534c28412cf5a9
emaxlele <emaxlele97@gmail.com>
2024-12-01 23:59:53 +0100

    fix error build

--- src/vite-env.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="vite/client" />
-
-// Puoi anche aggiungere variabili personalizzate per l'ambiente
-interface ImportMetaEnv {
-    // Aggiungi altre variabili d'ambiente personalizzate qui
-}
